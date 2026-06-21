aa30b919914652efcdca7bcbd870076adc88bb73
Joel Young <young.wjh@gmail.com>
2026-06-21 13:20:26 -0400

    initialize react

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      {/* Your component logic here */}
+    </div>
+  );
+}
+
+export default MyComponent;
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
